003a207735f43f21679f3f99937e5566ae658e1f
MostafaAmin07 <mostafa.amen07@gmail.com>
2018-10-16 19:41:17 +0200

    Better results than before Added another applyCrossover function with different method (The best are the selected children or parents) Added formulateChildren just to delete the duplicate code from the added applyCrossover function

--- src/com/company/GeneticAlgorithm.java
+++ src/com/company/GeneticAlgorithm.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import javafx.util.Pair;
 import org.knowm.xchart.QuickChart;
 import org.knowm.xchart.SwingWrapper;
 import org.knowm.xchart.XYChart;
@@ -7,6 +8,7 @@ import org.knowm.xchart.XYSeries;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Random;
 
 public class GeneticAlgorithm {
@@ -31,29 +33,30 @@ public class GeneticAlgorithm {
         bestValueIndex = -1;
     }
 
-    public void evolvePopulation(ArrayList<Item> items, Integer maxWeight) {
-        int bestPossibleValue = Knapsack.ComputeKnapsack(items, maxWeight);
+    public void evolvePopulation(Integer maxWeight) {
+        int bestPossibleValue = Knapsack.ComputeKnapsack(Main.items, maxWeight);
         for (int i = 0; i < MAX_GENERATION; i++)
             yData2[i] = bestPossibleValue;
         //generate random Population
-        generateRandomPopulation(items.size());
+        generateRandomPopulation(Main.items.size());
         for (int i = 0; i < MAX_GENERATION; i++) {
 //            System.out.println(population);
             //calculate fitness
-            ArrayList<Integer> fitnessValues = getFitness(items, maxWeight);
+            ArrayList<Integer> fitnessValues = getFitness(maxWeight);
 //            System.out.println(fitnessValues);
             //selection
             ArrayList<ArrayList<Boolean>> selected = selectParents(fitnessValues);
             //crossover
-            ArrayList<ArrayList<Boolean>> newPopulation = applyCrossover(selected);
+//            ArrayList<ArrayList<Boolean>> newPopulation = applyCrossover(selected);
+            ArrayList<ArrayList<Boolean>> newPopulation = applyCrossover(selected, maxWeight);
             //mutation
             applyMutation(newPopulation);
 //            System.out.println(newPopulation);
             population = newPopulation;
-            yData[i] = Collections.max(getFitness(items, maxWeight));
+            yData[i] = Collections.max(getFitness(maxWeight));
             xData[i] = i + 1;
         }
-        ArrayList<Integer> fitnessValues = getFitness(items, maxWeight);
+        ArrayList<Integer> fitnessValues = getFitness(maxWeight);
 //        System.out.println(population);
         bestValue = Collections.max(fitnessValues);
         bestValueIndex = fitnessValues.indexOf(bestValue);
@@ -88,20 +91,20 @@ public class GeneticAlgorithm {
         return chromosome;
     }
 
-    private ArrayList<Integer> getFitness(ArrayList<Item> items, Integer maxWeight) {
+    private ArrayList<Integer> getFitness(Integer maxWeight) {
         ArrayList<Integer> fitnessValues = new ArrayList<>();
         for (int i = 0; i < POPULATION_SIZE; i++) {
-            fitnessValues.add(calculateFitness(items, population.get(i), maxWeight));
+            fitnessValues.add(calculateFitness(population.get(i), maxWeight));
         }
         return fitnessValues;
     }
 
-    private Integer calculateFitness(ArrayList<Item> items, ArrayList<Boolean> chromosome, Integer maxWeight) {
+    private Integer calculateFitness(ArrayList<Boolean> chromosome, Integer maxWeight) {
         Integer value = 0, weight = 0;
         for (int i = 0; i < chromosome.size(); i++) {
             if (chromosome.get(i)) {
-                value += items.get(i).getValue();
-                weight += items.get(i).getWeight();
+                value += Main.items.get(i).getValue();
+                weight += Main.items.get(i).getWeight();
             }
         }
         if (weight > maxWeight) {
@@ -134,6 +137,7 @@ public class GeneticAlgorithm {
         return selected;
     }
 
+    //Standard method (children replace parents)
     private ArrayList<ArrayList<Boolean>> applyCrossover(ArrayList<ArrayList<Boolean>> parents) {
         ArrayList<ArrayList<Boolean>> newPopulation = new ArrayList<>();
         int crossoverPoint, chromosomeSize = parents.get(0).size();
@@ -142,15 +146,7 @@ public class GeneticAlgorithm {
             if (random.nextDouble() <= CROSSOVER_PROBABILITY) {
                 ArrayList<Boolean> firstChild = new ArrayList<>();
                 ArrayList<Boolean> secondChild = new ArrayList<>();
-                for (int j = 0; j < chromosomeSize; j++) {
-                    if (j < crossoverPoint) {
-                        firstChild.add(parents.get(i).get(j));
-                        secondChild.add(parents.get(i + 1).get(j));
-                    } else {
-                        firstChild.add(parents.get(i + 1).get(j));
-                        secondChild.add(parents.get(i).get(j));
-                    }
-                }
+                formulateChildren(parents, i, firstChild, secondChild, chromosomeSize, crossoverPoint);
                 newPopulation.add(firstChild);
                 newPopulation.add(secondChild);
             } else {
@@ -162,6 +158,58 @@ public class GeneticAlgorithm {
             newPopulation.add(population.get(POPULATION_SIZE - 1));
         }
         return newPopulation;
+    }
+
+    //choosing the best 2 of the parents and their children
+    private ArrayList<ArrayList<Boolean>> applyCrossover(ArrayList<ArrayList<Boolean>> parents, Integer maxWeight) {
+        ArrayList<ArrayList<Boolean>> newPopulation = new ArrayList<>();
+        int crossoverPoint, chromosomeSize = parents.get(0).size();
+        for (int i = 0; i < POPULATION_SIZE - 1; i += 2) {
+            crossoverPoint = random.nextInt(parents.size() - 2) + 1;
+            if (random.nextDouble() <= CROSSOVER_PROBABILITY) {
+                ArrayList<Boolean> firstChild = new ArrayList<>();
+                ArrayList<Boolean> secondChild = new ArrayList<>();
+                formulateChildren(parents, i, firstChild, secondChild, chromosomeSize, crossoverPoint);
+                ArrayList<Pair<ArrayList<Boolean>, Integer>> pickupLine = new ArrayList<>();
+                pickupLine.add(new Pair<>(firstChild, calculateFitness(firstChild, maxWeight)));
+                pickupLine.add(new Pair<>(secondChild, calculateFitness(secondChild, maxWeight)));
+                pickupLine.add(new Pair<>(parents.get(i), calculateFitness(parents.get(i), maxWeight)));
+                pickupLine.add(new Pair<>(parents.get(i + 1), calculateFitness(parents.get(i + 1), maxWeight)));
+                Collections.sort(pickupLine, new Comparator<Pair<ArrayList<Boolean>, Integer>>() {
+                    @Override
+                    public int compare(Pair<ArrayList<Boolean>, Integer> o1, Pair<ArrayList<Boolean>, Integer> o2) {
+                        if (o1.getValue() > o2.getValue()) {
+                            return -1;
+                        } else if (o1.getValue().equals(o2.getValue())) {
+                            return 0;
+                        } else {
+                            return 1;
+                        }
+                    }
+                });
+                newPopulation.add(pickupLine.get(0).getKey());
+                newPopulation.add(pickupLine.get(1).getKey());
+            } else {
+                newPopulation.add(parents.get(i));
+                newPopulation.add(parents.get(i + 1));
+            }
+        }
+        if (newPopulation.size() < POPULATION_SIZE) {
+            newPopulation.add(population.get(POPULATION_SIZE - 1));
+        }
+        return newPopulation;
+    }
+
+    private void formulateChildren(ArrayList<ArrayList<Boolean>> parents, Integer parentIndex, ArrayList<Boolean> firstChild, ArrayList<Boolean> secondChild, Integer chromosomeSize, Integer crossoverPoint) {
+        for (int j = 0; j < chromosomeSize; j++) {
+            if (j < crossoverPoint) {
+                firstChild.add(parents.get(parentIndex).get(j));
+                secondChild.add(parents.get(parentIndex + 1).get(j));
+            } else {
+                firstChild.add(parents.get(parentIndex + 1).get(j));
+                secondChild.add(parents.get(parentIndex).get(j));
+            }
+        }
     }
 
     private void applyMutation(ArrayList<ArrayList<Boolean>> newPopulation) {
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    public static ArrayList<Item> items;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Integer testCases = input.nextInt();
         Integer numberOfItems, maxWeight;
-        ArrayList<Item> items = new ArrayList<>();
+        items = new ArrayList<>();
         int index = 1;
         GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm();
         while (testCases > 0) {
@@ -18,7 +19,7 @@ public class Main {
             for (int i = 0; i < numberOfItems; i++) {
                 items.add(new Item(input.nextInt(), input.nextInt()));
             }
-            geneticAlgorithm.evolvePopulation(items, maxWeight);
+            geneticAlgorithm.evolvePopulation(maxWeight);
             System.out.println("Case: " + index++ + " " + geneticAlgorithm.getBestValue());
             items.clear();
             testCases--;
